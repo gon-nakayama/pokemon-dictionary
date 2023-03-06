611dc5dfad064aff9412e9dfabbd397d927e10a3
gon-nakayama <tknb77you+forgithub@gmail.com>
2023-03-07 08:37:04 +0900

    トランスレータの改良とデザイン修正

--- src/lib/translator/index.ts
+++ src/lib/translator/index.ts
@@ -1,9 +1,41 @@
-export const translatePokemonType = (type: "grass" | "poison") => {
+export const translatePokemonType = (type: string) => {
   switch (type) {
-    case "grass":
-      return "くさ";
+    case "normal":
+      return "ノーマル";
+    case "fighting":
+      return "かくとう";
+    case "flying":
+      return "ひこう";
     case "poison":
       return "どく";
+    case "ground":
+      return "じめん";
+    case "rock":
+      return "いわ";
+    case "bug":
+      return "むし";
+    case "ghost":
+      return "ゴースト";
+    case "steel":
+      return "はがね";
+    case "fire":
+      return "ほのお";
+    case "water":
+      return "みず";
+    case "grass":
+      return "くさ";
+    case "electric":
+      return "でんき";
+    case "psychic":
+      return "エスパー";
+    case "ice":
+      return "こおり";
+    case "dragon":
+      return "ドラゴン";
+    case "dark":
+      return "あく";
+    case "fairy":
+      return "フェアリー";
     default:
       return "なし";
   }
--- src/pages/pokemon/[id].tsx
+++ src/pages/pokemon/[id].tsx
@@ -10,27 +10,54 @@ type PokemonProps = {
 const PokemonDetail = ({ pokemon }: PokemonProps) => {
   return (
     <>
-      <h1>{pokemon.name}のページです</h1>
-      <p>分類：{pokemon.genus}</p>
-      <p>
-        タイプ：：
-        {pokemon.types
-          .map((t: { type: { name: string } }) =>
-            translatePokemonType(t.type.name as "grass" | "poison")
-          )
-          .join(" ")}
-      </p>
-      <p>
-        高さ： {pokemon.height}m 重さ： {pokemon.weight}kg
-      </p>
-      <p>特性：{pokemon.flavorText}</p>
-      <Image
-        className="animate-slide-in-bottom"
-        src={pokemon.image}
-        alt={pokemon.name}
-        width={200}
-        height={200}
-      />
+      <div className="mx-auto mt-8 w-full max-w-sm rounded-lg border border-gray-200 bg-white shadow dark:border-gray-700 dark:bg-gray-800">
+        <div className="flex items-center justify-center">
+          <span className="mt-4 text-3xl font-bold text-gray-900 dark:text-white">
+            {pokemon.name}
+          </span>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Image
+            className="animate-slide-in-bottom"
+            src={pokemon.image}
+            alt={pokemon.name}
+            width={200}
+            height={200}
+          />
+        </div>
+        <div className="flex items-center justify-center">
+          <span className="mt-3 text-2xl font-bold text-gray-500 dark:text-white">
+            分類：{pokemon.genus}
+          </span>
+        </div>
+        <div className="text-left">
+          <div className="mt-2.5 mb-5 flex items-center justify-center">
+            <span className="text-2xl font-bold text-gray-500 dark:text-white">
+              タイプ：
+            </span>
+            {pokemon.types.map((t: { type: { name: string } }, index) => (
+              <span
+                key={index}
+                className="mr-2 ml-3 rounded bg-blue-100 px-2.5 py-0.5 text-xs font-semibold text-blue-800 dark:bg-blue-200 dark:text-blue-800"
+              >
+                {translatePokemonType(t.type.name)}
+              </span>
+            ))}
+          </div>
+        </div>
+        <div className="px-5 pb-5">
+          <a href="#">
+            <h5 className="text-base font-semibold tracking-tight text-gray-900 dark:text-white">
+              {pokemon.flavorText}
+            </h5>
+          </a>
+        </div>
+      </div>
     </>
   );
 };
