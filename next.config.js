52ac2867d7ba4835a38c48376395354f99536eb4
youhei0177 <tknb77you@gmail.com>
2023-03-06 21:38:37 +0900

    Revert "ポケモン図鑑参照機能"
    This reverts commit d117eacb1688f064ce2edcf96dd9324a4d3858d5.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  images: {
-    domains: ["raw.githubusercontent.com"],
-  },
-};
+  reactStrictMode: true,
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
